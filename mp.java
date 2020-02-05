3436b42daba6d3e913bb5fea4b4de695cb894857
Shakir Masaari <35549045+shakir85@users.noreply.github.com>
2020-02-05 15:14:44 -0600

    Create mp.java

---
+++ mp.java
@@ -0,0 +1,27 @@
+public class MapSubset {
+	
+	public static void main(String[] args) {
+		int count=0;
+		Map<String, String> mp1 = new HashMap<>();
+		Map<String, String> mp2 = new HashMap<>();
+		mp1.put("1", "foo");
+		mp1.put("2", "bar");
+		mp1.put("3", "wow");
+		
+		mp2.put("1","foo");
+		mp2.put("2", "bar");
+		mp2.put("3", "zoo");
+		
+    //put all the keys in a set
+		Set<String> s1 = mp1.keySet(); 
+		Set<String> s2 = mp2.keySet();
+		
+    //Logic
+		for(String i : s1)
+			for(String j : s2)
+				if(mp1.get(i).equals(mp2.get(j))) 
+					System.out.println("Map 1 has: " +mp1.get(i) + " | Map 2 has: " + (mp2.get(j)));
+					count++;
+		System.out.println("match =" + count);
+	}
+}
